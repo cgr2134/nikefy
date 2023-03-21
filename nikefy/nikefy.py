d1b464b9c2584d0e27d86c03ac1110f5b239674c
cgr2134 <cgr2134@columbia.edu>
2023-03-21 17:52:26 -0400

    updated nikefy.py

--- nikefy/nikefy.py
+++ nikefy/nikefy.py
@@ -1,17 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_url(url):
+    return requests.get(url)
+    
+def get_html_parser(response):
+    return BeautifulSoup(response.content, 'html.parser')
 
 def get_product_info(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
+    response = get_url(url)
+    soup = get_html_parser(response)
+
     product_info = []
     products = soup.find_all('div', {'class': 'product-card__body'})
-    
+     
     for product in products:
         name = product.find('div', {'class': 'product-card__title'}).text.strip()
         price = product.find('div', {'class': 'product-price'}).text.strip()
-        product_info.append({'name': name, 'price': price})
+        product_info.append({'Product Name': name, 'Price': price})
 
-    return product_info
+    data = pd.DataFrame(product_info)
+    
+    return data
+    
 
