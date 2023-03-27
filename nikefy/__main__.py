2c6ec027aa8839f3099fb8a62dba474fd97bee34
cgr2134 <cgr2134@columbia.edu>
2023-03-27 02:53:46 -0400

    hw3v2

--- nikefy/__main__.py
+++ nikefy/__main__.py
@@ -1,7 +1,9 @@
-from nikefy import get_product_info
+from .nikefy import *
 
 if __name__ == "__main__":
     url = 'https://www.nike.com/w/mens-shoes-nik1zy7ok'
-    product_info = get_product_info(url)
-    print(product_info)
+    nike_products = get_nike_products(url)
+    print(nike_products)
+    sorted_nike_products = sort_nike_products(nike_products, sort_order='desc')
+    print(sorted_nike_products)
 
