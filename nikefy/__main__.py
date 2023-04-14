f510522284ff456f29b8c858261713b02ed786d0
cgr2134 <cgr2134@columbia.edu>
2023-04-14 16:15:43 -0400

    minor change

--- nikefy/__main__.py
+++ nikefy/__main__.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     url = 'https://www.nike.com/w/mens-shoes-nik1zy7ok'
     nike_products = get_nike_products(url)
     print(nike_products)
-    sorted_nike_products = sort_nike_products(nike_products, sort_order='desc')
+    sorted_nike_products = sort_nike_products(nike_products, sort_order='asc')
     print(sorted_nike_products)
